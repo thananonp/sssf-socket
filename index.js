80f2a35fd9916a569221d12059679e16289813fc
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-04-12 12:13:19 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-
-    socket.on('disconnect', () => {
-        console.log('a user disconnected', socket.id);
+io.on("connection", (socket) => {
+    console.log("a user connected", socket.id);
+    socket.join('global');
+    socket.on("disconnect", () => {
+        console.log("a user disconnected", socket.id);
     });
 
-    socket.on('chat message', (msg) => {
-        console.log('message: ', msg);
-        io.emit('chat message', msg);
+    socket.on("chat message", (user, channel, msg) => {
+        console.log("message: " + user + " @ " + channel + ": " + msg);
+        io.to(channel).emit("chat message", user, channel, msg);
+    });
+
+    socket.on("join", (channel) => {
+        console.log("user " + socket.id + " joined channel " + channel);
+        socket.leave('global');
+        socket.join(channel);
+    })
+
+    socket.on("leave", (channel) => {
+        console.log("user " + socket.id + " joined channel global");
+        socket.leave(channel);
+        socket.join('global');
+    })
+
+    socket.on("command", (msg) => {
+        console.log("command: ", msg);
+        socket.emit("command", msg);
+    });
+
+    socket.on("self message", (msg) => {
+        console.log("selfmessage: ", msg);
+        socket.emit("self message", msg);
     });
 });
 
 http.listen(3000, () => {
-    console.log('listening on port 3000');
+    console.log("listening on port 3000");
 });
---
+++ public/chat.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const socket = io();
+
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let user = document.getElementById("nickname").value;
+    if (user == "") {
+        user = "anonymous";
+    }
+    let channel = document.getElementById("channel").textContent;
+    const inp = document.getElementById("m");
+    if (inp.value.startsWith("/", 0)) {
+        socket.emit("command", inp.value, channel);
+    } else {
+        socket.emit("chat message", user, channel, inp.value);
+    }
+    inp.value = "";
+});
+
+let updateChannel = (channel) => {
+    document.getElementById("channel").innerText = channel;
+};
+
+function serverMessage(msg) {
+    socket.emit("self message", msg);
+}
+
+socket.on("command", (msg, channel) => {
+    console.log("command issued: " + msg);
+    if (msg.startsWith("/join ")) {
+        let newchannel = msg.replace("/join ", "");
+        socket.emit("join", newchannel, channel);
+        updateChannel(newchannel);
+    } else if (msg.startsWith("/leave")) {
+        let newchannel = "global";
+        socket.emit("leave", channel);
+        updateChannel(newchannel);
+    } else if (msg.startsWith("/help")) {
+        serverMessage(
+            "Nice try hackerman!"
+            // "<b><u>COMMANDS</u></b></br>" +
+            // "<tt>/join channelname</tt> join a channel</br>" +
+            // "<tt>/leave</tt> leave a channel"
+        );
+    } else {
+        serverMessage(
+            'Error: command "' +
+            (msg.split(" ", [0]) == "" ? msg : msg.split(" ", [0])) +
+            ' what did you type in here man there is no such command!'
+        );
+    }
+});
+
+socket.on("chat message", (user, channel, msg) => {
+    const item = document.createElement("li");
+    item.innerHTML = "<b>" + user + "</b>: " + msg;
+    document.getElementById("messages").appendChild(item);
+});
+
+socket.on("self message", (msg) => {
+    const item = document.createElement("li");
+    item.innerHTML = msg;
+    document.getElementById("messages").appendChild(item);
+});
+
+updateChannel("global");
